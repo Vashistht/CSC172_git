7393e29bbc816241b8fa1f77b9567a394970eea4
Vashisth <vtiwari2@u.rochester.edu>
2022-02-06 17:16:52 -0500

    f

--- Project1/MakeMove.java
+++ Project1/MakeMove.java
@@ -1,182 +1,182 @@
-import java.util.Arrays;
-
-public class MakeMove {
-
-    public static int[][]currentBoard = new int[4][4];
-    public static int numMoves;
-
-//    say our array of the board is called array2D
-
-    //2D array copy method
-    public static int[][] cloneBoard(){
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                currentBoard[i][j] = array2D[i][j];
-            }
-        }
-        return currentBoard;
-    }
-
-
-    //The methods below all shift elements in the array according to the desired direction
-    public static void moveLeft(int[][] array){
-        int[][] tempArray = array;
-        int tempValue;
-        for(int i = 0; i < tempArray.length; i++){
-            for(int j = 3; j >= 1; j--){
-                if((tempArray[i][j] != 0 ) && (tempArray[i][j-1] == 0)){
-                    tempValue = tempArray[i][j-1];
-                    tempArray[i][j-1] = tempArray[i][j];
-                    tempArray[i][j] = tempValue;
-                }
-            }
-        }
-    }
-
-    public static void moveRight(int[][] array){
-        int[][] tempArray = array;
-        int tempValue;
-        for(int i = 0; i < tempArray.length; i++){
-            for(int j = 0 ; j < 3; j++){
-                if((tempArray[i][j] != 0 && tempArray[i][j+1] == 0)){
-                    tempValue = tempArray[i][j+1];
-                    tempArray[i][j+1] = tempArray[i][j];
-                    tempArray[i][j] = tempValue;
-                }
-            }
-        }
-    }
-
-    public static void moveUp(int[][] array){
-        int[][] tempArray = array;
-        int tempValue;
-        for(int i = 1; i <= 3 ; i++){
-            for(int j = 0; j < 4; j++){
-                if(tempArray[i][j] != 0 && tempArray[i-1][j] == 0){
-                    tempValue = tempArray[i - 1][j];
-                    tempArray[i-1][j] = tempArray[i][j];
-                    tempArray[i][j] = tempValue;
-                }
-            }
-        }
-    }
-
-    public static void moveDown(int[][] array){
-        int[][] tempArray = array;
-        int tempValue;
-        for(int i = 2; i >= 0; i--){
-            for(int j = 0; j < 4; j ++){
-                if(tempArray[i][j] != 0 && tempArray[i + 1][j] == 0){
-                    tempValue = tempArray[i + 1][j];
-                    tempArray[i+1][j] = tempArray[i][j];
-                    tempArray[i][j] = tempValue;
-                }
-            }
-        }
-    }
-
-    public static void addingTiles(int[][] board, String dir) {
-        int[][] tempArray = board;
-        switch (dir) {
-            case "a":
-                //movedArray temporarily stores the current board for comparison, if the two arrays
-                //are equal, it means no movement was made, therefore it was an invalid move
-                cloneBoard();
-                //Checks if numbers are the same, if they are then you add them
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (tempArray[i][j] == tempArray[i][j + 1]) {
-                            tempArray[i][j] += tempArray[i][j + 1];
-                            tempArray[i][j + 1] = 0;
-                        }
-                    }
-                }
-                //Loops through array to check if there is a zero to the left of the element,
-                //If there is, you swap them, essentially shifting to the left
-                for (int i = 0; i < tempArray.length; i++) {
-                    for (int j = 0; j < tempArray[i].length; j++) {
-                        moveLeft(board);
-                    }
-                }
-
-                if(!Arrays.deepEquals(currentBoard, tempArray)) {
-                    MakeBoard.updateBoard();
-                    numMoves++;
-                }
-                break;
-
-            case "d":
-                cloneBoard();
-                //Check if numbers are the same, if they are, add them
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 3; j > 0; j--) {
-                        if(tempArray[i][j] == tempArray[i][j - 1]){
-                            tempArray[i][j] += tempArray[i][j-1];
-                            tempArray[i][j-1] = 0;
-                        }
-                    }
-                }
-                //Loops through array to check if there is a zero to the right of the element,
-                //If there is, you swap them, essentially shifting to the right
-                for(int i = 0; i < tempArray.length; i++){
-                    for(int j = 0; j < tempArray[i].length; j++){
-                        moveRight(board);
-                    }
-                }
-
-                if(!Arrays.deepEquals(currentBoard, tempArray)) {
-                    MakeBoard.updateBoard();
-                    numMoves++;
-                }
-
-                break;
-            case "w":
-                cloneBoard();
-                //Check if numbers in the row above are the same, if they are, add them
-                for (int i = 1; i <= 3; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        if(tempArray[i][j] == tempArray[i - 1][j]){
-                            tempArray[i-1][j] += tempArray[i][j];
-                            tempArray[i][j] = 0;
-                        }
-                    }
-                }
-                //Loops through array to check if there is a zero above the element,
-                //If there is, you swap them, essentially shifting "above"
-                for (int i = 0; i < tempArray.length; i++) {
-                    for (int j = 0; j < tempArray[i].length; j++) {
-                        moveUp(board);
-                    }
-                }
-
-                if(!Arrays.deepEquals(currentBoard, tempArray)) {
-                    MakeBoard.updateBoard();
-                    numMoves++;
-                }
-                break;
-            case "s":
-                cloneBoard();
-                //down
-                for (int i = 3; i > 0; i--) {
-                    for (int j = 0; j < 4; j++) {
-                        if(tempArray[i][j] == tempArray[i - 1][j]){
-                            tempArray[i][j] += tempArray[i-1][j];
-                            tempArray[i-1][j] = 0;
-                        }
-                    }
-                }
-                //Loops through array to check if there is a zero below the element,
-                //If there is, you swap them, essentially shifting "down"
-                for (int i = 0; i < tempArray.length; i++) {
-                    for (int j = 0; j < tempArray[i].length; j++) {
-                        moveDown(board);
-                    }
-                }
-                if(!Arrays.deepEquals(currentBoard, tempArray)) {
-                    Board.updateBoard();
-                    numMoves++;
-                }
-                break;
-        }
-    }
-}
+//import java.util.Arrays;
+//
+//public class MakeMove {
+//
+//    public static int[][]currentBoard = new int[4][4];
+//    public static int numMoves;
+//
+////    say our array of the board is called array2D
+//
+//    //2D array copy method
+//    public static int[][] cloneBoard(){
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                currentBoard[i][j] = array2D[i][j];
+//            }
+//        }
+//        return currentBoard;
+//    }
+//
+//
+//    //The methods below all shift elements in the array according to the desired direction
+//    public static void moveLeft(int[][] array){
+//        int[][] tempArray = array;
+//        int tempValue;
+//        for(int i = 0; i < tempArray.length; i++){
+//            for(int j = 3; j >= 1; j--){
+//                if((tempArray[i][j] != 0 ) && (tempArray[i][j-1] == 0)){
+//                    tempValue = tempArray[i][j-1];
+//                    tempArray[i][j-1] = tempArray[i][j];
+//                    tempArray[i][j] = tempValue;
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void moveRight(int[][] array){
+//        int[][] tempArray = array;
+//        int tempValue;
+//        for(int i = 0; i < tempArray.length; i++){
+//            for(int j = 0 ; j < 3; j++){
+//                if((tempArray[i][j] != 0 && tempArray[i][j+1] == 0)){
+//                    tempValue = tempArray[i][j+1];
+//                    tempArray[i][j+1] = tempArray[i][j];
+//                    tempArray[i][j] = tempValue;
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void moveUp(int[][] array){
+//        int[][] tempArray = array;
+//        int tempValue;
+//        for(int i = 1; i <= 3 ; i++){
+//            for(int j = 0; j < 4; j++){
+//                if(tempArray[i][j] != 0 && tempArray[i-1][j] == 0){
+//                    tempValue = tempArray[i - 1][j];
+//                    tempArray[i-1][j] = tempArray[i][j];
+//                    tempArray[i][j] = tempValue;
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void moveDown(int[][] array){
+//        int[][] tempArray = array;
+//        int tempValue;
+//        for(int i = 2; i >= 0; i--){
+//            for(int j = 0; j < 4; j ++){
+//                if(tempArray[i][j] != 0 && tempArray[i + 1][j] == 0){
+//                    tempValue = tempArray[i + 1][j];
+//                    tempArray[i+1][j] = tempArray[i][j];
+//                    tempArray[i][j] = tempValue;
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void addingTiles(int[][] board, String dir) {
+//        int[][] tempArray = board;
+//        switch (dir) {
+//            case "a":
+//                //movedArray temporarily stores the current board for comparison, if the two arrays
+//                //are equal, it means no movement was made, therefore it was an invalid move
+//                cloneBoard();
+//                //Checks if numbers are the same, if they are then you add them
+//                for (int i = 0; i < 4; i++) {
+//                    for (int j = 0; j < 3; j++) {
+//                        if (tempArray[i][j] == tempArray[i][j + 1]) {
+//                            tempArray[i][j] += tempArray[i][j + 1];
+//                            tempArray[i][j + 1] = 0;
+//                        }
+//                    }
+//                }
+//                //Loops through array to check if there is a zero to the left of the element,
+//                //If there is, you swap them, essentially shifting to the left
+//                for (int i = 0; i < tempArray.length; i++) {
+//                    for (int j = 0; j < tempArray[i].length; j++) {
+//                        moveLeft(board);
+//                    }
+//                }
+//
+//                if(!Arrays.deepEquals(currentBoard, tempArray)) {
+//                    MakeBoard.updateBoard();
+//                    numMoves++;
+//                }
+//                break;
+//
+//            case "d":
+//                cloneBoard();
+//                //Check if numbers are the same, if they are, add them
+//                for (int i = 0; i < 4; i++) {
+//                    for (int j = 3; j > 0; j--) {
+//                        if(tempArray[i][j] == tempArray[i][j - 1]){
+//                            tempArray[i][j] += tempArray[i][j-1];
+//                            tempArray[i][j-1] = 0;
+//                        }
+//                    }
+//                }
+//                //Loops through array to check if there is a zero to the right of the element,
+//                //If there is, you swap them, essentially shifting to the right
+//                for(int i = 0; i < tempArray.length; i++){
+//                    for(int j = 0; j < tempArray[i].length; j++){
+//                        moveRight(board);
+//                    }
+//                }
+//
+//                if(!Arrays.deepEquals(currentBoard, tempArray)) {
+//                    MakeBoard.updateBoard();
+//                    numMoves++;
+//                }
+//
+//                break;
+//            case "w":
+//                cloneBoard();
+//                //Check if numbers in the row above are the same, if they are, add them
+//                for (int i = 1; i <= 3; i++) {
+//                    for (int j = 0; j < 4; j++) {
+//                        if(tempArray[i][j] == tempArray[i - 1][j]){
+//                            tempArray[i-1][j] += tempArray[i][j];
+//                            tempArray[i][j] = 0;
+//                        }
+//                    }
+//                }
+//                //Loops through array to check if there is a zero above the element,
+//                //If there is, you swap them, essentially shifting "above"
+//                for (int i = 0; i < tempArray.length; i++) {
+//                    for (int j = 0; j < tempArray[i].length; j++) {
+//                        moveUp(board);
+//                    }
+//                }
+//
+//                if(!Arrays.deepEquals(currentBoard, tempArray)) {
+//                    MakeBoard.updateBoard();
+//                    numMoves++;
+//                }
+//                break;
+//            case "s":
+//                cloneBoard();
+//                //down
+//                for (int i = 3; i > 0; i--) {
+//                    for (int j = 0; j < 4; j++) {
+//                        if(tempArray[i][j] == tempArray[i - 1][j]){
+//                            tempArray[i][j] += tempArray[i-1][j];
+//                            tempArray[i-1][j] = 0;
+//                        }
+//                    }
+//                }
+//                //Loops through array to check if there is a zero below the element,
+//                //If there is, you swap them, essentially shifting "down"
+//                for (int i = 0; i < tempArray.length; i++) {
+//                    for (int j = 0; j < tempArray[i].length; j++) {
+//                        moveDown(board);
+//                    }
+//                }
+//                if(!Arrays.deepEquals(currentBoard, tempArray)) {
+//                    Board.updateBoard();
+//                    numMoves++;
+//                }
+//                break;
+//        }
+//    }
+//}
--- Project1/project01.java
+++ Project1/project01.java
@@ -1,37 +1,65 @@
-package dp;
-
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class project01 {
-	
+	private int numValidMoves = 0;
+	private int[][] arrayBoard = new int[4][4];
+
+	public int[][] getArrayBoard() {
+		return arrayBoard;
+	}
+
+	public int getNumValidMoves() {
+		return numValidMoves;
+	}
+
 	public static void print2Darray(int[][] array) {
 		String intAsString = "";
-		
+
+		System.out.print(" ");
+		for (int i = 0; i < 15; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+
 		for (int i = 0; i < 4; i++) {
+			System.out.print("|");
 			for (int j = 0; j < 4; j++) {
-				if (array[i][j] == 0) {
-					System.out.printf("%-10s", "*");
+				if (j != 3) {
+					if (array[i][j] == 0) {
+						intAsString = "*";
+					} else {
+						intAsString = Integer.toString(array[i][j]);
+					}
+					System.out.printf("%-5s", intAsString);
 				} else {
-					intAsString = Integer.toString(array[i][j]);
-					System.out.printf("%-10s", intAsString);
+					if (array[i][j] == 0) {
+						intAsString = "*";
+					} else {
+						intAsString = Integer.toString(array[i][j]);
+					}
+					System.out.print(intAsString);
 				}
 			}
-			System.out.print("\n" + "\n" + "\n" + "\n");
+			System.out.printf("%-5s", "|");
+			System.out.println();
 		}
+		System.out.print(" ");
+		for (int i = 0; i < 15; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
 	}
-	
-	public static int[][] createRandomArray(){
+
+	public int[][] createRandomArray(){
 		Random rand = new Random();
-		int[][] buildArray = new int[4][4];
 		int[] determineIf4 = new int[2];
 		int[] location1 = new int[2];
 		int[] location2 = new int[2];
-		
+
 		determineIf4[0] = rand.nextInt(4);
 		determineIf4[1] = rand.nextInt(4);
-		
+
 		location1[0] = rand.nextInt(4);
 		location1[1] = rand.nextInt(4);
 		location2[0] = rand.nextInt(4);
@@ -40,166 +68,229 @@ public class project01 {
 			location2[0] = rand.nextInt(4);
 			location2[1] = rand.nextInt(4);
 		}
-		
+
 		if (determineIf4[0] == 0) {
-			buildArray[location1[0]][location1[1]] = 4;
+			arrayBoard[location1[0]][location1[1]] = 4;
 		} else {
-			buildArray[location1[0]][location1[1]] = 2;
+			arrayBoard[location1[0]][location1[1]] = 2;
 		}
-		
+
 		if (determineIf4[1] == 0) {
-			buildArray[location2[0]][location2[1]] = 4;
+			arrayBoard[location2[0]][location2[1]] = 4;
 		} else {
-			buildArray[location2[0]][location2[1]] = 2;
+			arrayBoard[location2[0]][location2[1]] = 2;
 		}
-		
+
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if ((i != location1[0]) || (j != location1[1])) {
 					if ((i != location2[0]) || (j != location2[1])) {
-						buildArray[i][j] = 0;
+						arrayBoard[i][j] = 0;
 					}
 				}
 			}
 		}
-		return buildArray;
+		return arrayBoard;
 	}
-	
-	public static boolean placeRandomNumber(int[][] array) {
+
+	public boolean placeRandomNumber() {
 		Random rand = new Random();
 		boolean canPlaceNum = false;
 		int[] locationOfNum = new int[2];
 		int determineIf4 = rand.nextInt(4);
-		
+
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (array[i][j] == 0) {
+				if (arrayBoard[i][j] == 0) {
 					canPlaceNum = true;
 				}
 			}
 		}
-		
+
 		if (canPlaceNum == true) {
 			locationOfNum[0] = rand.nextInt(4);
 			locationOfNum[1] = rand.nextInt(4);
-			while (array[locationOfNum[0]][locationOfNum[1]] != 0) {
+			while (arrayBoard[locationOfNum[0]][locationOfNum[1]] != 0) {
 				locationOfNum[0] = rand.nextInt(4);
 				locationOfNum[1] = rand.nextInt(4);
 			}
 			if (determineIf4 == 0) {
-				array[locationOfNum[0]][locationOfNum[1]] = 4;
+				arrayBoard[locationOfNum[0]][locationOfNum[1]] = 4;
 			} else {
-				array[locationOfNum[0]][locationOfNum[1]] = 2;
+				arrayBoard[locationOfNum[0]][locationOfNum[1]] = 2;
 			}
 		}
-		
+
 		return canPlaceNum;
 	}
-	
-	public static int[][] moveInDirection(String dir, int[][] array) {		
+
+	public int[][] moveInDirection(String dir) {
 		int[][] compressedArray = new int[4][4];
-		
+
 		switch (dir) {
-		case "a":
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 4; j++) {
-					compressedArray[i][j] = 0;
-					for (int k = j; k < 4; k++) {
-						if (compressedArray[i][j] == 0) {
-							compressedArray[i][j] += array[i][k];
-							array[i][k] = 0;
-						} else if (array[i][k] == 0) {
-							compressedArray[i][j] += array[i][k];
-						} else if (compressedArray[i][j] == array[i][k]) {
-							compressedArray[i][j] += array[i][k];
-							array[i][k] = 0;
-							break;
-						} else {
-							break;
+			case "a":
+				for (int i = 0; i < 4; i++) {
+					for (int j = 0; j < 4; j++) {
+						compressedArray[i][j] = 0;
+						for (int k = j; k < 4; k++) {
+							if (compressedArray[i][j] == 0) {
+								compressedArray[i][j] += arrayBoard[i][k];
+								arrayBoard[i][k] = 0;
+							} else if (arrayBoard[i][k] == 0) {
+								compressedArray[i][j] += arrayBoard[i][k];
+							} else if (compressedArray[i][j] == arrayBoard[i][k]) {
+								compressedArray[i][j] += arrayBoard[i][k];
+								arrayBoard[i][k] = 0;
+								break;
+							} else {
+								break;
+							}
 						}
 					}
 				}
-			}
-			break;
-		case "s":
-			for (int i = 0; i < 4; i++) {
-				for (int j = 3; j >= 0; j--) {
-					compressedArray[i][j] = 0;
-					for (int k = j; k >= 0; k--) {
-						if (compressedArray[j][i] == 0) {
-							compressedArray[j][i] += array[k][i];
-							array[k][i] = 0;
-						} else if (array[k][i] == 0) {
-							compressedArray[j][i] += array[k][i];
-						} else if (compressedArray[j][i] == array[k][i]) {
-							compressedArray[j][i] += array[k][i];
-							array[k][i] = 0;
-							break;
-						} else {
-							break;
+				numValidMoves += 1;
+				break;
+			case "s":
+				for (int i = 0; i < 4; i++) {
+					for (int j = 3; j >= 0; j--) {
+						compressedArray[j][i] = 0;
+						for (int k = j; k >= 0; k--) {
+							if (compressedArray[j][i] == 0) {
+								compressedArray[j][i] += arrayBoard[k][i];
+								arrayBoard[k][i] = 0;
+							} else if (arrayBoard[k][i] == 0) {
+								compressedArray[j][i] += arrayBoard[k][i];
+							} else if (compressedArray[j][i] == arrayBoard[k][i]) {
+								compressedArray[j][i] += arrayBoard[k][i];
+								arrayBoard[k][i] = 0;
+								break;
+							} else {
+								break;
+							}
 						}
 					}
 				}
-			}
-			break;
-		case "d":
-			for (int i = 0; i < 4; i++) {
-				for (int j = 3; j >= 0; j--) {
-					compressedArray[i][j] = 0;
-					for (int k = j; k >= 0; k--) {
-						if (compressedArray[i][j] == 0) {
-							compressedArray[i][j] += array[i][k];
-							array[i][k] = 0;
-						} else if (array[i][k] == 0) {
-							compressedArray[i][j] += array[i][k];
-						} else if (compressedArray[i][j] == array[i][k]) {
-							compressedArray[i][j] += array[i][k];
-							array[i][k] = 0;
-							break;
-						} else {
-							break;
+				numValidMoves += 1;
+				break;
+			case "d":
+				for (int i = 0; i < 4; i++) {
+					for (int j = 3; j >= 0; j--) {
+						compressedArray[i][j] = 0;
+						for (int k = j; k >= 0; k--) {
+							if (compressedArray[i][j] == 0) {
+								compressedArray[i][j] += arrayBoard[i][k];
+								arrayBoard[i][k] = 0;
+							} else if (arrayBoard[i][k] == 0) {
+								compressedArray[i][j] += arrayBoard[i][k];
+							} else if (compressedArray[i][j] == arrayBoard[i][k]) {
+								compressedArray[i][j] += arrayBoard[i][k];
+								arrayBoard[i][k] = 0;
+								break;
+							} else {
+								break;
+							}
 						}
 					}
 				}
-			}
-			break;
-		case "w":
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 4; j++) {
-					compressedArray[i][j] = 0;
-					for (int k = j; k < 4; k++) {
-						if (compressedArray[j][i] == 0) {
-							compressedArray[j][i] += array[k][i];
-							array[k][i] = 0;
-						} else if (array[k][i] == 0) {
-							compressedArray[j][i] += array[k][i];
-						} else if (compressedArray[j][i] == array[k][i]) {
-							compressedArray[j][i] += array[k][i];
-							array[k][i] = 0;
-							break;
-						} else {
-							break;
+				numValidMoves += 1;
+				break;
+			case "w":
+				for (int i = 0; i < 4; i++) {
+					for (int j = 0; j < 4; j++) {
+						compressedArray[j][i] = 0;
+						for (int k = j; k < 4; k++) {
+							if (compressedArray[j][i] == 0) {
+								compressedArray[j][i] += arrayBoard[k][i];
+								arrayBoard[k][i] = 0;
+							} else if (arrayBoard[k][i] == 0) {
+								compressedArray[j][i] += arrayBoard[k][i];
+							} else if (compressedArray[j][i] == arrayBoard[k][i]) {
+								compressedArray[j][i] += arrayBoard[k][i];
+								arrayBoard[k][i] = 0;
+								break;
+							} else {
+								break;
+							}
 						}
 					}
+				}
+				numValidMoves += 1;
+				break;
+		}
+
+		boolean sameBoard = true;
+
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (arrayBoard[i][j] != compressedArray[i][j]) {
+					sameBoard = false;
+					System.out.println("Vash");
+					arrayBoard[i][j] = compressedArray[i][j];
 				}
 			}
 		}
-		
-		return compressedArray;
+
+		if (sameBoard == true) {
+			numValidMoves -= 1;
+			System.out.println("Not a Valid Move.");
+		}
+
+		return arrayBoard;
 	}
-	
+
+	public int getMaxElement() {
+		int max = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (arrayBoard[i][j] > max) {
+					max = arrayBoard[i][j];
+				}
+			}
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[][] myArray = createRandomArray();
-		print2Darray(myArray);
+		project01 myGame = new project01();
+		boolean continueGame = true;
+
+		myGame.createRandomArray();
+		print2Darray(myGame.getArrayBoard());
 		System.out.println();
-		
-		while (true) {
+
+		while (continueGame == true) {
 			System.out.println("Enter a dir:");
 			String userInput = scan.nextLine();
-			myArray = moveInDirection(userInput, myArray);
-			placeRandomNumber(myArray);
-			print2Darray(myArray);
+			switch (userInput) {
+				case ("r"):
+					myGame.createRandomArray();
+					print2Darray(myGame.getArrayBoard());
+					break;
+				case ("q"):
+					System.out.println("You quit.");
+					continueGame = false;
+					break;
+				case ("a"):
+				case ("s"):
+				case ("d"):
+				case ("w"):
+					myGame.moveInDirection(userInput);
+					myGame.placeRandomNumber();
+					print2Darray(myGame.getArrayBoard());
+					System.out.print("Number of Valid Moves: ");
+					System.out.println(myGame.getNumValidMoves());
+//System.out.print("Max Number:");
+//System.out.println(myGame.getMaxElement());
+					break;
+				default:
+					System.out.println("Not a Valid Move.");
+			}
+			if (myGame.getMaxElement() == 16 ) {
+				System.out.println("You won!");
+				continueGame = false;
+			}
 		}
 	}
 
