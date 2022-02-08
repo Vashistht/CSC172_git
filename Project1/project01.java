b18adfe8ce572fea24f17ee64c8216820e9209b6
lmb13a <90067571+lmb13a@users.noreply.github.com>
2022-02-08 09:44:20 -0500

    Add files via upload

--- Project1/project01.java
+++ Project1/project01.java
@@ -1,27 +1,29 @@
+package dp;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class project01 {
 	private int numValidMoves = 0;
 	private int[][] arrayBoard = new int[4][4];
-
+	
 	public int[][] getArrayBoard() {
 		return arrayBoard;
 	}
-
+	
 	public int getNumValidMoves() {
 		return numValidMoves;
 	}
-
+	
 	public static void print2Darray(int[][] array) {
 		String intAsString = "";
-
+		
 		System.out.print(" ");
 		for (int i = 0; i < 15; i++) {
 			System.out.print("-");
 		}
 		System.out.println();
-
+		
 		for (int i = 0; i < 4; i++) {
 			System.out.print("|");
 			for (int j = 0; j < 4; j++) {
@@ -50,16 +52,16 @@ public class project01 {
 		}
 		System.out.println();
 	}
-
+	
 	public int[][] createRandomArray(){
 		Random rand = new Random();
 		int[] determineIf4 = new int[2];
 		int[] location1 = new int[2];
 		int[] location2 = new int[2];
-
+		
 		determineIf4[0] = rand.nextInt(4);
 		determineIf4[1] = rand.nextInt(4);
-
+		
 		location1[0] = rand.nextInt(4);
 		location1[1] = rand.nextInt(4);
 		location2[0] = rand.nextInt(4);
@@ -68,19 +70,19 @@ public class project01 {
 			location2[0] = rand.nextInt(4);
 			location2[1] = rand.nextInt(4);
 		}
-
+		
 		if (determineIf4[0] == 0) {
 			arrayBoard[location1[0]][location1[1]] = 4;
 		} else {
 			arrayBoard[location1[0]][location1[1]] = 2;
 		}
-
+		
 		if (determineIf4[1] == 0) {
 			arrayBoard[location2[0]][location2[1]] = 4;
 		} else {
 			arrayBoard[location2[0]][location2[1]] = 2;
 		}
-
+		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if ((i != location1[0]) || (j != location1[1])) {
@@ -92,13 +94,13 @@ public class project01 {
 		}
 		return arrayBoard;
 	}
-
+	
 	public boolean placeRandomNumber() {
 		Random rand = new Random();
 		boolean canPlaceNum = false;
 		int[] locationOfNum = new int[2];
 		int determineIf4 = rand.nextInt(4);
-
+		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if (arrayBoard[i][j] == 0) {
@@ -106,7 +108,7 @@ public class project01 {
 				}
 			}
 		}
-
+		
 		if (canPlaceNum == true) {
 			locationOfNum[0] = rand.nextInt(4);
 			locationOfNum[1] = rand.nextInt(4);
@@ -120,173 +122,162 @@ public class project01 {
 				arrayBoard[locationOfNum[0]][locationOfNum[1]] = 2;
 			}
 		}
-
+		
 		return canPlaceNum;
 	}
-
-	public int[][] moveInDirection(String dir) {
+	
+	public int[][] moveInDirection(String dir) {		
 		int[][] compressedArray = new int[4][4];
-
+		
 		switch (dir) {
-			case "a":
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						compressedArray[i][j] = 0;
-						for (int k = j; k < 4; k++) {
-							if (compressedArray[i][j] == 0) {
-								compressedArray[i][j] += arrayBoard[i][k];
-								arrayBoard[i][k] = 0;
-							} else if (arrayBoard[i][k] == 0) {
-								compressedArray[i][j] += arrayBoard[i][k];
-							} else if (compressedArray[i][j] == arrayBoard[i][k]) {
-								compressedArray[i][j] += arrayBoard[i][k];
-								arrayBoard[i][k] = 0;
-								break;
-							} else {
-								break;
-							}
+		case "a":
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					compressedArray[i][j] = 0;
+					for (int k = j; k < 4; k++) {
+						if (compressedArray[i][j] == 0) {
+							compressedArray[i][j] += arrayBoard[i][k];
+							arrayBoard[i][k] = 0;
+						} else if (arrayBoard[i][k] == 0) {
+							compressedArray[i][j] += arrayBoard[i][k];
+						} else if (compressedArray[i][j] == arrayBoard[i][k]) {
+							compressedArray[i][j] += arrayBoard[i][k];
+							arrayBoard[i][k] = 0;
+							break;
+						} else {
+							break;
 						}
 					}
 				}
-				numValidMoves += 1;
-				break;
-			case "s":
-				for (int i = 0; i < 4; i++) {
-					for (int j = 3; j >= 0; j--) {
-						compressedArray[j][i] = 0;
-						for (int k = j; k >= 0; k--) {
-							if (compressedArray[j][i] == 0) {
-								compressedArray[j][i] += arrayBoard[k][i];
-								arrayBoard[k][i] = 0;
-							} else if (arrayBoard[k][i] == 0) {
-								compressedArray[j][i] += arrayBoard[k][i];
-							} else if (compressedArray[j][i] == arrayBoard[k][i]) {
-								compressedArray[j][i] += arrayBoard[k][i];
-								arrayBoard[k][i] = 0;
-								break;
-							} else {
-								break;
-							}
+			}
+			numValidMoves += 1;
+			break;
+		case "s":
+			for (int i = 0; i < 4; i++) {
+				for (int j = 3; j >= 0; j--) {
+					compressedArray[j][i] = 0;
+					for (int k = j; k >= 0; k--) {
+						if (compressedArray[j][i] == 0) {
+							compressedArray[j][i] += arrayBoard[k][i];
+							arrayBoard[k][i] = 0;
+						} else if (arrayBoard[k][i] == 0) {
+							compressedArray[j][i] += arrayBoard[k][i];
+						} else if (compressedArray[j][i] == arrayBoard[k][i]) {
+							compressedArray[j][i] += arrayBoard[k][i];
+							arrayBoard[k][i] = 0;
+							break;
+						} else {
+							break;
 						}
 					}
 				}
-				numValidMoves += 1;
-				break;
-			case "d":
-				for (int i = 0; i < 4; i++) {
-					for (int j = 3; j >= 0; j--) {
-						compressedArray[i][j] = 0;
-						for (int k = j; k >= 0; k--) {
-							if (compressedArray[i][j] == 0) {
-								compressedArray[i][j] += arrayBoard[i][k];
-								arrayBoard[i][k] = 0;
-							} else if (arrayBoard[i][k] == 0) {
-								compressedArray[i][j] += arrayBoard[i][k];
-							} else if (compressedArray[i][j] == arrayBoard[i][k]) {
-								compressedArray[i][j] += arrayBoard[i][k];
-								arrayBoard[i][k] = 0;
-								break;
-							} else {
-								break;
-							}
+			}
+			numValidMoves += 1;
+			break;
+		case "d":
+			for (int i = 0; i < 4; i++) {
+				for (int j = 3; j >= 0; j--) {
+					compressedArray[i][j] = 0;
+					for (int k = j; k >= 0; k--) {
+						if (compressedArray[i][j] == 0) {
+							compressedArray[i][j] += arrayBoard[i][k];
+							arrayBoard[i][k] = 0;
+						} else if (arrayBoard[i][k] == 0) {
+							compressedArray[i][j] += arrayBoard[i][k];
+						} else if (compressedArray[i][j] == arrayBoard[i][k]) {
+							compressedArray[i][j] += arrayBoard[i][k];
+							arrayBoard[i][k] = 0;
+							break;
+						} else {
+							break;
 						}
 					}
 				}
-				numValidMoves += 1;
-				break;
-			case "w":
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						compressedArray[j][i] = 0;
-						for (int k = j; k < 4; k++) {
-							if (compressedArray[j][i] == 0) {
-								compressedArray[j][i] += arrayBoard[k][i];
-								arrayBoard[k][i] = 0;
-							} else if (arrayBoard[k][i] == 0) {
-								compressedArray[j][i] += arrayBoard[k][i];
-							} else if (compressedArray[j][i] == arrayBoard[k][i]) {
-								compressedArray[j][i] += arrayBoard[k][i];
-								arrayBoard[k][i] = 0;
-								break;
-							} else {
-								break;
-							}
+			}
+			numValidMoves += 1;
+			break;
+		case "w":
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					compressedArray[j][i] = 0;
+					for (int k = j; k < 4; k++) {
+						if (compressedArray[j][i] == 0) {
+							compressedArray[j][i] += arrayBoard[k][i];
+							arrayBoard[k][i] = 0;
+						} else if (arrayBoard[k][i] == 0) {
+							compressedArray[j][i] += arrayBoard[k][i];
+						} else if (compressedArray[j][i] == arrayBoard[k][i]) {
+							compressedArray[j][i] += arrayBoard[k][i];
+							arrayBoard[k][i] = 0;
+							break;
+						} else {
+							break;
 						}
 					}
 				}
-				numValidMoves += 1;
-				break;
+			}
+			numValidMoves += 1;
+			break;
 		}
-
-		boolean sameBoard = true;
-
+		//checkIfSameBoard(arrayBoard, compressedArray);
 		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (arrayBoard[i][j] != compressedArray[i][j]) {
-					sameBoard = false;
-					System.out.println("Vash");
-					arrayBoard[i][j] = compressedArray[i][j];
-				}
+				arrayBoard[i][j] = compressedArray[i][j];
 			}
 		}
-
-		if (sameBoard == true) {
-			numValidMoves -= 1;
-			System.out.println("Not a Valid Move.");
-		}
-
+		
 		return arrayBoard;
 	}
-
+	
 	public int getMaxElement() {
-		int max = 0;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (arrayBoard[i][j] > max) {
-					max = arrayBoard[i][j];
-				}
-			}
-		}
-		return max;
+	    int max = 0;
+	    for (int i = 0; i < 4; i++) {
+	        for (int j = 0; j < 4; j++) {
+	            if (arrayBoard[i][j] > max) {
+	                max = arrayBoard[i][j];
+	            }
+	        }
+	    }
+	    return max;
 	}
-
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		project01 myGame = new project01();
 		boolean continueGame = true;
-
+		
 		myGame.createRandomArray();
 		print2Darray(myGame.getArrayBoard());
 		System.out.println();
-
+		
 		while (continueGame == true) {
 			System.out.println("Enter a dir:");
 			String userInput = scan.nextLine();
 			switch (userInput) {
-				case ("r"):
-					myGame.createRandomArray();
-					print2Darray(myGame.getArrayBoard());
-					break;
-				case ("q"):
-					System.out.println("You quit.");
-					continueGame = false;
-					break;
-				case ("a"):
-				case ("s"):
-				case ("d"):
-				case ("w"):
-					myGame.moveInDirection(userInput);
-					myGame.placeRandomNumber();
-					print2Darray(myGame.getArrayBoard());
-					System.out.print("Number of Valid Moves: ");
-					System.out.println(myGame.getNumValidMoves());
-//System.out.print("Max Number:");
-//System.out.println(myGame.getMaxElement());
-					break;
-				default:
-					System.out.println("Not a Valid Move.");
-			}
+			case ("r"):
+				myGame.createRandomArray();
+				print2Darray(myGame.getArrayBoard());
+				break;
+			case ("q"):
+				System.out.println("You quit.");
+				continueGame = false;
+				break;
+			case ("a"):
+			case ("s"):
+			case ("d"):
+			case ("w"):
+				myGame.moveInDirection(userInput);
+				myGame.placeRandomNumber();
+				print2Darray(myGame.getArrayBoard());
+				System.out.print("Number of Valid Moves: ");
+				System.out.println(myGame.getNumValidMoves());
+				//System.out.print("Max Number:");
+				//System.out.println(myGame.getMaxElement());
+				break;
+			default:
+				System.out.println("Not a Valid Move.");
+			}	
 			if (myGame.getMaxElement() == 16 ) {
 				System.out.println("You won!");
 				continueGame = false;
