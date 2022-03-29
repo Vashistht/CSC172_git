471e7121eee404b4753e535de0a99ec725d90fe1
Vashisth <vtiwari2@u.rochester.edu>
2022-03-28 22:47:47 -0400

    Update Main.java

--- vtiwari2_Lab5/src/Main.java
+++ vtiwari2_Lab5/src/Main.java
@@ -6,36 +6,39 @@ import java.util.Scanner;
 
 public class Main{
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) { //throws FileNotFoundException 
         Trie<Integer, String> nodes = new Trie<>();
         File commands= new File(args[0]);
-        Scanner strings = new Scanner(commands);
 
-        while(strings.hasNextLine()){
-            String commandSingle = strings.nextLine();
-            /** split when one or many white spaces and add them as elements of the list
-            // first element decides the action,
-            // second (if present) where that action would take place 
-            */
-            String[] commandsSeparated = commandSingle.trim().split("\\s+"); 
+        try (Scanner strings = new Scanner(commands)) {
+            while(strings.hasNextLine()){
+                String commandSingle = strings.nextLine();
+                /** split when one or many white spaces and add them as elements of the list
+                // first element decides the action,
+                // second (if present) where that action would take place 
+                */
+                String[] commandsSeparated = commandSingle.trim().split("\\s+"); 
 
-            if(commandsSeparated[0].toLowerCase().equals("insert")){
-                nodes.insert(commandsSeparated[1]);
+                if(commandsSeparated[0].toLowerCase().equals("insert")){
+                    nodes.insert(commandsSeparated[1]);
+                } else if(commandsSeparated[0].toLowerCase().equals("search")){
 
-            } else if(commandsSeparated[0].toLowerCase().equals("search")){
+                } else if(commandsSeparated[0].toLowerCase().equals("height")){
 
-            } else if(commandsSeparated[0].toLowerCase().equals("height")){
+                } else if(commandsSeparated[0].toLowerCase().equals("size")){
 
-            } else if(commandsSeparated[0].toLowerCase().equals("size")){
+                } else if(commandsSeparated[0].toLowerCase().equals("print")){
+                    // nodes.trieToList(Trie.node); trietoList needs more work
 
-            } else if(commandsSeparated[0].toLowerCase().equals("print")){
-                // nodes.trieToList(Trie.node); trietoList needs more work
+                } else if(commandsSeparated[0].toLowerCase().equals("smallest")){
 
-            } else if(commandsSeparated[0].toLowerCase().equals("smallest")){
+                } else if(commandsSeparated[0].toLowerCase().equals("largest")){
 
-            } else if(commandsSeparated[0].toLowerCase().equals("largest")){
-
+                }
             }
+        }
+        catch (Exception e) {
+            System.out.println(e);  
         }
     }
 
